e49af27177001cbe6a023c0edfe23e0e4d5b43fd
Romain Gueffier <r.gueffier.pro@gmail.com>
2021-06-11 21:42:52 +0200

    Docker env for production and cors fixes

--- client/src/contexts/SocketProvider.js
+++ client/src/contexts/SocketProvider.js
@@ -12,7 +12,10 @@ export function SocketProvider({ id, children }) {
     const [socket, setSocket] = useState()
 
     useEffect(() => {
-        const newSocket = io('http://localhost:5000', { query: { id } })
+        const newSocket = io('https://socket.parrots.thenest.freeddns.org', { 
+            transports: [ "websocket" ],
+            query: { id } 
+        })
         setSocket(newSocket)
 
         return () => newSocket.close()
